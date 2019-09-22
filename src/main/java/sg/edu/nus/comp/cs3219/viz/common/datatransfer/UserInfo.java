c3ac43680a2d815c40dca4a02d16d21554303a9a
jeffgan96 <jeffganmr@hotmail.com>
2019-09-22 20:16:23 +0800

    edits

--- src/main/java/sg/edu/nus/comp/cs3219/viz/common/datatransfer/UserInfo.java
+++ src/main/java/sg/edu/nus/comp/cs3219/viz/common/datatransfer/UserInfo.java
@@ -6,6 +6,16 @@ public class UserInfo {
 
     private String userNickname;
 
+    private String userId;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
     public String getUserEmail() {
         return userEmail;
     }
