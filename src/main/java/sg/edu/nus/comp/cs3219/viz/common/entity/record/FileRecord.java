3a895827b11951669fee38025b9b0f6627219401
jeffgan96 <jeffganmr@hotmail.com>
2019-09-24 01:34:37 +0800

    minor edit to filerecord

--- src/main/java/sg/edu/nus/comp/cs3219/viz/common/entity/record/FileRecord.java
+++ src/main/java/sg/edu/nus/comp/cs3219/viz/common/entity/record/FileRecord.java
@@ -1,9 +1,13 @@
 package sg.edu.nus.comp.cs3219.viz.common.entity.record;
 
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
 import java.io.Serializable;
+import java.util.UUID;
 
 @Exportable(name = "File Record", nameInDB = "file_record")
 @Entity
@@ -11,23 +15,27 @@ import java.io.Serializable;
 public class FileRecord implements Serializable {
 
     @Id
-    private String userId;
+    @Column(name = "user_id")
+    private UUID userId;
 
     @Id
+    @Column(name = "file_id")
     private String fileId;
 
+    @Column(name = "file_name")
     private String fileName;
 
-    public FileRecord(String userId, String fileId) {
+    public FileRecord(UUID userId, String fileId, String fileName) {
         this.userId = userId;
         this.fileId = fileId;
+        this.fileName = fileName;
     }
 
-    public String getUserId() {
+    public UUID getUserId() {
         return userId;
     }
 
-    public void setUserId(String userId) {
+    public void setUserId(UUID userId) {
         this.userId = userId;
     }
 
