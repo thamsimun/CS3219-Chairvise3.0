14beded4e2c405e6da6cc26ed4c003f51af9b65d
jeffgan96 <jeffganmr@hotmail.com>
2019-09-27 16:10:09 +0800

    change author record to use user_id + file_id

--- src/main/java/sg/edu/nus/comp/cs3219/viz/storage/repository/AuthorRecordRepository.java
+++ src/main/java/sg/edu/nus/comp/cs3219/viz/storage/repository/AuthorRecordRepository.java
@@ -2,13 +2,18 @@ package sg.edu.nus.comp.cs3219.viz.storage.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import sg.edu.nus.comp.cs3219.viz.common.entity.record.AuthorRecord;
+import sg.edu.nus.comp.cs3219.viz.common.entity.record.FileId;
 
 import java.util.List;
 
 public interface AuthorRecordRepository extends JpaRepository<AuthorRecord, Long> {
 
-    List<AuthorRecord> findByDataSetEquals(String dataSet);
+    List<AuthorRecord> findAllByFileRecordFileIdUserIdEquals(long userId);
 
-    void deleteAllByDataSetEquals(String dataSet);
+    //delete all by a user id
+    void deleteAllByFileRecordFileIdUserIdEquals(long userId);
+
+    //delete all by a specific file
+    void deleteAllByFileRecordFileIdEquals(FileId fileId);
 
 }
