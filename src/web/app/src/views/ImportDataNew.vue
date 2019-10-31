cd6105559aec0cecd19bf320c9d585ab75435b28
thamsimun <e0201281@u.nus.edu>
2019-10-31 14:15:48 +0800

    edit importDataNew to fit sidebar

--- src/web/app/src/views/ImportDataNew.vue
+++ src/web/app/src/views/ImportDataNew.vue
@@ -38,62 +38,6 @@
         </div>
       </el-col>
     </el-container>
-<!--  <el-container>-->
-<!--    <el-container>-->
-<!--      <el-aside class="sidebar" width="250px">-->
-<!--        <DataSideBar/>-->
-<!--      </el-aside>-->
-<!--    </el-container>-->
-<!--    <el-container>-->
-<!--      <el-col>-->
-<!--        <mapping-tool-new v-if='isReady' ref='mapTool'></mapping-tool-new>-->
-<!--        <div v-else class="upload">-->
-<!--          <h1>Select record type</h1>-->
-<!--          <div class='card-container' v-for='dbSchema in dbSchemas' v-bind:key='dbSchema.tableName'>-->
-<!--            <div v-on:click='select(dbSchema)'>-->
-<!--              <el-card class='box-card' shadow='hover'>-->
-<!--                <h1>{{ dbSchema.name }}</h1>-->
-<!--                <ul>-->
-<!--                  <li v-for='field in dbSchema.fieldMetaDataList' v-bind:key='field.name'>-->
-<!--                    {{ field.name }}-->
-<!--                  </li>-->
-<!--                </ul>-->
-<!--              </el-card>-->
-<!--            </div>-->
-<!--          </div>-->
-<!--          <div>-->
-<!--            <el-upload drag action=''-->
-<!--                       :auto-upload='false'-->
-<!--                       :show-file-list='false'-->
-<!--                       :multiple='false'-->
-<!--                       :on-change='fileUploadHandler'>-->
-<!--              <i class='el-icon-upload'></i>-->
-<!--              <div class='el-upload__text'>Drop file here or <em>click to upload</em></div>-->
-<!--              <div class='el-upload__tip' slot='tip'>Please upload .csv files with filed names.</div>-->
-<!--            </el-upload>-->
-<!--          </div>-->
-<!--          <div class='template-container'>-->
-<!--            <p>CHOOSE TEMPLATE</p>-->
-<!--          </div>-->
-<!--          <div>-->
-<!--            <h2>Pick columns</h2>-->
-<!--            <ul>-->
-<!--              <li v-for='(field, index) in pool' v-bind:key='index' v-on:click='addToSelected(field)'>-->
-<!--                {{ field }}-->
-<!--              </li>-->
-<!--            </ul>-->
-<!--            <h2>Selected</h2>-->
-<!--            <ul>-->
-<!--              <li v-for='(field, index) in selected' v-bind:key='index' v-on:click='addToPool(field)'>-->
-<!--                {{ field }}-->
-<!--              </li>-->
-<!--            </ul>-->
-<!--          </div>-->
-<!--          <el-button v-on:click='nextClicked'>Next</el-button>-->
-<!--        </div>-->
-<!--      </el-col>-->
-<!--    </el-container>-->
-<!--  </el-container>-->
   </el-container>
 </template>
 
@@ -169,6 +113,7 @@
     display: flex;
     justify-content: center;
     align-items: center;
+    padding-left: 210px;
   }
 
   h1 {
