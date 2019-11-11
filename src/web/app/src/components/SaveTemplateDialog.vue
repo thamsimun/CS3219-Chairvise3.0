9dedc10568ceb7bafe70cef1254b2e08a1a0fe2d
Wai Lun <wailun@u.nus.edu>
2019-11-11 22:26:54 +0800

    Create dialog form for saving a template

---
+++ src/web/app/src/components/SaveTemplateDialog.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+  <el-button type='text' @click='dialogFormVisible=true'>Save this template</el-button>
+    <el-dialog :visible.sync='dialogFormVisible' title='Save template'>
+      <el-form>
+        <el-form-item label='Name' label-width='120px'>
+          <el-input v-model='name' autocomplete='off'></el-input>
+        </el-form-item>
+        <el-form-item label='Description' label-width='120px'>
+          <el-input v-model='description' autocomplete='off'></el-input>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "SaveTemplateDialog",
+    data() {
+      return {
+        name: '',
+        description: '',
+        dialogFormVisible: false
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
